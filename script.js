57c09887870918a2d5864e2c6661e3e754e73e56
Shteinaseev <142624493+Shteinaseev@users.noreply.github.com>
2024-11-29 06:33:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,45 @@ var options = {
 };
 
 var typed = new Typed("#typed", options);
+
+
+var options1 = {
+    strings: ["Reflakcija.", "Prelamanje svetlosti."],
+    typeSpeed: 50, // Скорость печати
+    backSpeed: 0, // Скорость удаления (0 - не удалять)
+    loop: false, // Отключить цикличность
+    cursorChar: '|',  // Символ курсора
+    startDelay: 500,
+    showCursor: true 
+};
+
+var typed2 = new Typed("#typed2", options1);
+
+
+var options2 = {
+    strings: ["Konstrukcija lika kod sabirnih sočiva."],
+    typeSpeed: 50, // Скорость печати
+    backSpeed: 0, // Скорость удаления (0 - не удалять)
+    loop: false, // Отключить цикличность
+    cursorChar: '|',  // Символ курсора
+    startDelay: 500,
+    showCursor: true 
+};
+
+
+var typed3 = new Typed("#typed3", options2);
+
+var options3 = {
+    strings: ["Konstrukcija lika kod rasipnih sočiva."],
+    typeSpeed: 50, // Скорость печати
+    backSpeed: 0, // Скорость удаления (0 - не удалять)
+    loop: false, // Отключить цикличность
+    cursorChar: '|',  // Символ курсора
+    startDelay: 500,
+    showCursor: true 
+};
+
+
+var typed4 = new Typed("#typed4", options3);
+
+document.addEventListener('DOMContentLoaded', function() { const menuToggle = document.querySelector('.menu-toggle'); const navMenu = document.querySelector('menu'); menuToggle.addEventListener('click', function() { navMenu.classList.toggle('active'); }); });
